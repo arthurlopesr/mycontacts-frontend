6048c1382da80ab3cc5a4195243208e923b86422
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-05 16:58:48 -0300

    feat: select

---
+++ src/components/select.js
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  border: 2px solid #fff;
+  background: #FFF;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  padding: 16px;
+  outline: none;
+  margin-top: 24px;
+  font-size: 16px;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    transition: 0.2s ease-in;
+  }
+
+  & + & {
+    margin-top: 16px ;
+  }
+`;
--- src/pages/new-contact/NewContact.jsx
+++ src/pages/new-contact/NewContact.jsx
@@ -1,9 +1,26 @@
+import Input from '../../components/input';
 import { PageHeader } from '../../components/page-header/PageHeader';
+import Select from '../../components/select';
 
 export function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input
+        type="text"
+        placeholder="Nome"
+      />
+
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+
+    </>
   );
 }
