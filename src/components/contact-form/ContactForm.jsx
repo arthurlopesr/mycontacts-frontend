4eef55a3b5f1e522e4e021052198af71dbbddafd
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-07 18:32:19 -0300

    feat: useState for all inputs

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -8,30 +8,52 @@ import Button from '../button';
 
 export function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('');
-  return (
-    <Form>
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
 
+  function handleSubmit(event) {
+    console.log({
+      name, email, phone, category,
+    });
+
+    event.preventDefault();
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Input
-          value={name}
           placeholder="Nome"
+          value={name}
           onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
-      <FormGroup
-        error="O formato do e-mail é inválido."
-      >
-        <Input placeholder="E-mail" error />
+      <FormGroup>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="Categoria">Categorias</option>
           <option value="Instagram">Intagram</option>
+          <option value="Discod">Discord</option>
         </Select>
       </FormGroup>
 
